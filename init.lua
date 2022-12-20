d300404d383cf7d5d784de5597d20fa47e22e329
jack mcshane <jackrmcshane@gmail.com>
2022-12-20 09:26:06 -0500

    adds vim dir; sources wilder.vim file

--- init.lua
+++ init.lua
@@ -1,1 +1,14 @@
 require('lua-config')
+
+
+-- all code below sources vim files from 'vim' dir
+local vim_files = {
+    'wilder.vim',
+}
+
+
+for _, fname in ipairs(vim_files) do
+    local path = string.format("%s/vim/%s", vim.fn.stdpath("config"), fname)
+    local source_cmd = "source" .. path
+    vim.cmd(source_cmd)
+end
