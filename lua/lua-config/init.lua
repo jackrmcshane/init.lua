97d17f757caede0efea49039e7ea884b41d29313
jack mcshane <jackrmcshane@gmail.com>
2022-12-20 09:15:15 -0500

    incorporates autocommands.lua

--- lua/lua-config/init.lua
+++ lua/lua-config/init.lua
@@ -1,5 +1,6 @@
 require('lua-config.packer')
 require('lua-config.set')
 require('lua-config.mappings')
+require('lua-config.autocommands')
 require('lua-config.colorscheme')
 require('lua-config.plugins')
